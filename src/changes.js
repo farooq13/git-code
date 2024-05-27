4245bafc1ac28e47a403367bda091e22fa466c56
faruk <faruk@email.com>
2024-05-27 20:45:35 +0100

    version 3

--- src/changes.js
+++ src/changes.js
@@ -1,2 +1,2 @@
 console.log('changes.js');
-console.log('version 2');
+console.log('version 3');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('index.js');
-console.log('version 2');
+console.log('version 3');
+console.log('Testing...');
